2e00d1991f26e697234adb4fb95c5368c5e17cbb
Khandakerhub <billalkhandaker@yahoo.com>
2025-05-17 16:22:16 -0400

    chat button script move to js file

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
   alert('Thank you for contacting me! I will respond soon.');
   this.reset();
 });
+
+/* paste this line in verbatim */
+		window.formbutton = window.formbutton || function () { (formbutton.q = formbutton.q || []).push(arguments) };
+		/* customize formbutton below*/
+		formbutton("create", {
+			action: "https://formspree.io/f/xanovvwg",
+			title: "Contact Me:",
+			fields: [
+				{
+					type: "text",
+					label: "Name:",
+					name: "name",
+					required: true,
+					placeholder: "your name"
+				},
+				{
+					type: "email",
+					label: "Email:",
+					name: "email",
+					required: true,
+					placeholder: "your@email.com"
+				},
+				{
+					type: "textarea",
+					label: "Message:",
+					name: "message",
+					placeholder: "Your message",
+					required: true
+				},
+				{ type: "submit" }
+			],
+			styles: {
+				title: {
+					backgroundColor: "#0b598a"
+				},
+				button: {
+					backgroundColor: "#0a324b"
+				}
+			}
+		});
